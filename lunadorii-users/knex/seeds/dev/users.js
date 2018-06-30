8483bf015a69a165c946274817818cce28297aaf
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-07-01 01:38:30 +0700

    Fix server name

--- lunadorii-users/knex/seeds/dev/users.js
+++ lunadorii-users/knex/seeds/dev/users.js
@@ -5,11 +5,46 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('users').insert([
-        { first_name: 'Kevin', last_name: 'Hermawan', email: 'kevinhermawanx@gmail.com', bod: '1999-06-06', password: '$2y$12$TeIo.XWWP9EeAGSg7QJS.u0lqscwjaLEpLuO85zbhw4vgeTIJM9jm' },
-        { first_name: 'Rendi', last_name: 'Simamora', email: 'rendisimamora@gmail.com', bod: '1999-06-06', password: '$2y$12$XcMWB0qmnbgUIVMLpV7tuuShKxooo1x8ZR4ap85nJwAaqqquCiije' },
-        { first_name: 'Dicky', last_name: 'Perdian', email: 'dickyperdian@gmail.com', bod: '1999-06-06', password: '$2y$12$yAk4NYA5otPUsvg.vrtp6uylMWVFus7KbYF9UJv2A5wKrtzF1DcSW' },
-        { first_name: 'Alfan', last_name: 'Hiban', email: 'alfanhiban@gmail.com', bod: '1999-06-06', password: '$2y$12$CY4JMHCC472QvgSw9XsklOuswWSC84Ym9os6aJib.A.gw6YCCeaYq' },
-        { first_name: 'Isa', last_name: 'WK', email: 'isawk@gmail.com', bod: '1999-06-06', password: '$2y$12$x1urq81GQ/NbCtLYoqu9Re6en4Eu/tOJZ/X4ST/9lrFhhG6S6q6r2' }
+        {
+          first_name: 'Kevin',
+          last_name: 'Hermawan',
+          email: 'kevinhermawanx@gmail.com',
+          bod: '1999-06-06',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/20070808?s=460&v=4',
+          password: '$2b$10$5ST1MzQxEAvyNWjoO9iSyee05U0fnzFl4rSl7Ycs6ULw31VjhLrcW'
+        },
+        { 
+          first_name: 'Rendi',
+          last_name: 'Simamora',
+          email: 'rendisimamora@gmail.com',
+          bod: '1999-06-06',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/20070808?s=460&v=4',
+          password: '$2b$10$5ST1MzQxEAvyNWjoO9iSyee05U0fnzFl4rSl7Ycs6ULw31VjhLrcW'
+        },
+        {
+          first_name: 'Dicky',
+          last_name: 'Perdian',
+          email: 'dickyperdian@gmail.com',
+          bod: '1999-06-06',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/20070808?s=460&v=4',
+          password: '$2b$10$5ST1MzQxEAvyNWjoO9iSyee05U0fnzFl4rSl7Ycs6ULw31VjhLrcW'
+        },
+        { 
+          first_name: 'Alfan',
+          last_name: 'Hiban',
+          email: 'alfanhiban@gmail.com',
+          bod: '1999-06-06',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/20070808?s=460&v=4',
+          password: '$2b$10$5ST1MzQxEAvyNWjoO9iSyee05U0fnzFl4rSl7Ycs6ULw31VjhLrcW'
+        },
+        { 
+          first_name: 'Isa',
+          last_name: 'WK',
+          email: 'isawk@gmail.com',
+          bod: '1999-06-06',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/20070808?s=460&v=4',
+          password: '$2b$10$5ST1MzQxEAvyNWjoO9iSyee05U0fnzFl4rSl7Ycs6ULw31VjhLrcW'
+        }
       ])
     })
 }
