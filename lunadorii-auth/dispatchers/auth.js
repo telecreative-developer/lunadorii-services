a1fb69e05c2c2fbe9526a98d9709762c71617564
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-07-12 10:50:02 +0700

    Fix wishlist on best_seller

--- lunadorii-auth/dispatchers/auth.js
+++ lunadorii-auth/dispatchers/auth.js
@@ -98,3 +98,64 @@ exports.authFacebook = data => {
 		})
 		.catch(err => errorResponse(err, 500))
 }
+
+exports.authGoogle = data => {
+	return knex(users)
+		.where("email", data.email)
+		.limit(1)
+		.then(response => {
+			if (response.length) {
+				const check = !!response.filter(
+					rescheck => rescheck.provider === "google"
+				).length
+
+				if (check) {
+					const accessToken = jwt.sign(
+						{ id: response[0].id, role: "user" },
+						process.env.JWT_SECRET_KEY,
+						accessTokenObject
+					)
+					const refreshToken = jwt.sign(
+						{ id: response[0].id, role: "user" },
+						process.env.JWT_SECRET_KEY,
+						refreshTokenObject
+					)
+					return successResponse(
+						{ id: response[0].id, accessToken, refreshToken },
+						"Success Authenticate with Google",
+						201
+					)
+				} else {
+					return errorResponse("Email is already exist", 500)
+				}
+			} else {
+				return knex("users")
+					.insert({
+						first_name: res.first_name,
+						last_name: res.last_name,
+						email: res.email,
+						avatar_url: res.picture.data.url,
+						provider: "facebook"
+					})
+					.returning("id")
+					.then(id => {
+						const accessToken = jwt.sign(
+							{ id: parseInt(id.toString()), role: "user" },
+							process.env.JWT_SECRET_KEY,
+							accessTokenObject
+						)
+						const refreshToken = jwt.sign(
+							{ id: parseInt(id.toString()), role: "user" },
+							process.env.JWT_SECRET_KEY,
+							refreshTokenObject
+						)
+						return successResponse(
+							{ id: parseInt(id.toString()), accessToken, refreshToken },
+							"Success Authenticate with Facebook",
+							201
+						)
+					})
+					.catch(err => errorResponse(err, 500))
+			}
+		})
+}
