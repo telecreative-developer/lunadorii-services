2cae78e2500e1d0c1267fbd63c73430a3ffe540a
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-07-13 19:31:39 +0700

    Added city_id & province_id

--- lunadorii-users/definitions/addresses.js
+++ lunadorii-users/definitions/addresses.js
@@ -1,17 +1,21 @@
-const addressesDefinition = [{
-	user_address_id: {column: 'user_address_id', id: true},
-  label: {column: 'label'},
-	recepient: {column: 'recepient'},
-  phone: {column: 'phone'},
-	longitude: {column: 'longitude'},
-  latitude: {column: 'latitude'},
-  city: {column: 'city'},
-  province: {column: 'province'},
-  postal_code: {column: 'postal_code'},
-  detail_address: {column: 'detail_address'},
-  address_default: {column: 'address_default'},
-	created_at: {column: 'created_at'},
-	updated_at: {column: 'updated_at'}
-}]
+const addressesDefinition = [
+  {
+    user_address_id: { column: "user_address_id", id: true },
+    label: { column: "label" },
+    recepient: { column: "recepient" },
+    phone: { column: "phone" },
+    longitude: { column: "longitude" },
+    latitude: { column: "latitude" },
+    city_id: { column: "city_id", id: true },
+    city: { column: "city" },
+    province_id: { column: "province_id", id: true },
+    province: { column: "province" },
+    postal_code: { column: "postal_code" },
+    detail_address: { column: "detail_address" },
+    address_default: { column: "address_default" },
+    created_at: { column: "created_at" },
+    updated_at: { column: "updated_at" }
+  }
+]
 
 module.exports = addressesDefinition
