ade26c45f0127c180f45e478140c442928fd799a
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-07-16 10:47:59 +0700

    Fix search

--- lunadorii-search/dispatchers/search.js
+++ lunadorii-search/dispatchers/search.js
@@ -95,3 +95,104 @@ exports.search = attributes => {
 		)
 		.catch(err => console.log(err))
 }
+
+exports.searchLogged = attributes => {
+	return knex("products")
+		.where("product", "like", `%${attributes.payload}%`)
+		.where(
+			builder =>
+				attributes.subcategories &&
+				builder.whereIn(
+					"products.product_subcategory_id",
+					JSON.parse(attributes.subcategories)
+				)
+		)
+		.where(
+			builder =>
+				attributes.productBrand &&
+				builder.where("products.product_brand_id", attributes.productBrand)
+		)
+		.innerJoin(
+			"product_subcategories",
+			"products.product_subcategory_id",
+			"product_subcategories.product_subcategory_id"
+		)
+		.innerJoin(
+			"product_brands",
+			"products.product_brand_id",
+			"product_brands.product_brand_id"
+		)
+		.innerJoin(
+			"product_thumbnails",
+			"products.product_id",
+			"product_thumbnails.product_id"
+		)
+		.leftJoin(
+			"product_reviews",
+			"products.product_id",
+			"product_reviews.product_id"
+		)
+		.leftJoin("users", "product_reviews.id", "users.id")
+		.select(
+			"*",
+			"products.product_id as product_id",
+			"users.id as product_reviews_user_id",
+			"users.avatar_url as product_reviews_avatar_url",
+			"users.first_name as product_reviews_first_name",
+			"users.last_name as product_reviews_last_name",
+			"product_subcategories.product_subcategory_id as product_subcategory_id",
+			"product_brands.product_brand_id as product_brand_id",
+			"product_thumbnails.product_thumbnail_id as product_thumbnail_id",
+			"product_reviews.product_review_id as product_review_id",
+			"product_reviews.created_at as product_reviews_created_at",
+			"product_reviews.updated_at as product_reviews_updated_at"
+		)
+		.then(response =>
+			response.map(res => ({
+				...res,
+				product_reviews_avatar_url: res.product_reviews_avatar_url
+					? process.env.AWS_IMAGE_URL + res.product_reviews_avatar_url
+					: process.env.AWS_IMAGE_DEFAULT_URL
+			}))
+		)
+		.then(response => NestHydrationJS.nest(response, searchDefinition))
+		.then(response =>
+			knex("wishlist")
+				.where("id", id)
+				.then(res =>
+					response.map(rproduct => ({
+						...rproduct,
+						wishlisted: !!res.filter(
+							rwishlist => rwishlist.product_id === rproduct.product_id
+						).length
+					}))
+				)
+		)
+		.then(response =>
+			response.map(res => ({
+				...res,
+				product_rate: res.reviews.length
+					? res.reviews.map(r => r.review_rate).reduce((a, b) => a + b) /
+					  res.reviews.length
+					: 0
+			}))
+		)
+		.then(response => {
+			if (attributes.maxPrice && attributes.minPrice) {
+				return response.filter(
+					res =>
+						res.price <= attributes.maxPrice && res.price >= attributes.minPrice
+				)
+			}
+
+			return response
+		})
+		.then(response =>
+			successResponse(
+				response,
+				`Success Search (keyword: ${attributes.payload})`,
+				200
+			)
+		)
+		.catch(err => console.log(err))
+}
--- lunadorii-search/routes/v1/search.js
+++ lunadorii-search/routes/v1/search.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const Promise = require('bluebird')
+const express = require("express")
+const Promise = require("bluebird")
 const router = express.Router()
-const { search } = require('../../dispatchers/search')
+const { search, searchLogged } = require("../../dispatchers/search")
 
 // Search
-router.get('/search', (req, res) => {
-	Promise.try(() => search(req.query))
+router.get("/search", (req, res) => {
+	Promise.try(
+		() => (req.query.id ? searchLogged(req.query) : search(req.query))
+	)
 		.then(response => res.status(response.status).json(response))
-		.catch(err => console.log('Error on SEARCH', err))
+		.catch(err => console.log("Error on SEARCH", err))
 })
 
 module.exports = router
