931fee7126542ad832db92d26ddac23e27b1a1fd
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-07-23 18:29:01 +0700

    Added midtrans

--- lunadorii-orders/routes/v1/orders.js
+++ lunadorii-orders/routes/v1/orders.js
@@ -6,6 +6,7 @@ const authentication = require("../../middleware/authentication")
 const {
 	checkoutOrder,
 	getOrderHistory,
+	getOrderHistorySingle,
 	getOrderRecent
 } = require("../../dispatchers/order")
 
@@ -19,6 +20,13 @@ router.post("/order/checkout", authentication, (req, res) => {
 // Order history
 router.get("/order/history/:id", authentication, (req, res) => {
 	Promise.try(() => getOrderHistory(req.params.id))
+		.then(response => res.status(response.status).json(response))
+		.catch(err => console.log("Error on GET_ORDER_HISTORY", err))
+})
+
+// Order history single
+router.get("/order/history/single/:order_id", authentication, (req, res) => {
+	Promise.try(() => getOrderHistorySingle(req.params.order_id))
 		.then(response => res.status(response.status).json(response))
 		.catch(err => console.log("Error on GET_ORDER_HISTORY", err))
 })
