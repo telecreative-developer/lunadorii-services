285eeb2bd4d330322c61ca5e5e3413a401ff7335
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-07-27 16:47:10 +0700

    Added send link download

--- lunadorii-general/dispatchers/link.js
+++ lunadorii-general/dispatchers/link.js
@@ -1,29 +1,55 @@
 require("dotenv").config({ path: __dirname + "/./../../.env" })
+const Promise = require("bluebird")
 const express = require("express")
 const nodemailer = require("nodemailer")
 const mg = require("nodemailer-mailgun-transport")
+const { successResponse, errorResponse } = require("../responsers")
+const handlebars = require("handlebars")
+const fs = require("fs")
+const fsReadFileAsync = Promise.promisify(fs.readFile)
+const emailTemplateLinkDownload =
+	"/./../../lunadorii-email-templates/send-link-download.html"
 const authMg = {
 	auth: {
 		api_key: process.env.MAILGUN_API_KEY,
 		domain: process.env.MAILGUN_DOMAIN
 	}
 }
-const nodemailerMailgun = nodemailer.createTransport(mg(authMg))
+const nodemailerMailgun = Promise.promisifyAll(
+	nodemailer.createTransport(mg(authMg))
+)
+
+const readHTMLFile = path => {
+	return fsReadFileAsync(path, { encoding: "utf-8" })
+		.then(html => html)
+		.catch(err => err)
+}
+
+const nodemailerMailgunAsync = (email, template, data) => {
+	return nodemailerMailgun
+		.sendMail(mailOptions(email, template, data))
+		.then(res => successResponse(null, "Success Send Link", 200))
+		.catch(err => errorResponse("Internal Server Error", 500))
+}
+
+const mailOptions = (email, template, data) => {
+	const html = template(data)
+	return {
+		from: "no-reply@lunadorii.com",
+		to: email,
+		subject: "Download Aplikasi Lunadorii",
+		html: html
+	}
+}
 
 exports.sendLinkApp = email => {
-	return new Promise((resolve, reject) => {
-		return nodemailerMailgun.sendMail(
-			{
-				from: "no-reply@lunadorii.com",
-				to: email,
-				subject: "Download Aplikasi Lunadorii",
-				html: `<a href='http://54.169.224.248:3000/reset-password?token=${token}'><b>KLIK DISINI!</b></a>`
-			},
-			(err, info) => {
-				err
-					? reject(errorResponse("Internal Server Error", 500))
-					: resolve(null, "Success Send Link", 200)
-			}
-		)
-	})
+	return readHTMLFile(__dirname + emailTemplateLinkDownload)
+		.then(html => handlebars.compile(html))
+		.then(hbs => {
+			return nodemailerMailgunAsync(email, hbs, {
+				appstoreLink: "https://facebook.com",
+				playstoreLink: "https://twitter.com"
+			})
+		})
+		.catch(err => err)
 }
