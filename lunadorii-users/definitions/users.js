7306dd44d818449b6ee18b72714f27cace97b0fb
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-08-02 11:40:23 +0700

    Verified

--- lunadorii-users/definitions/users.js
+++ lunadorii-users/definitions/users.js
@@ -1,12 +1,15 @@
-const userDefinition = [{
-	id: {column: 'id', id: true},
-	first_name: {column: 'first_name'},
-	last_name: {column: 'last_name'},
-	avatar_url: {column: 'avatar_url'},
-	email: {column: 'email'},
-	provider: {column: 'provider'},
-	bod: {column: 'bod'},
-	joined_at: {column: 'created_at'}
-}]
+const userDefinition = [
+	{
+		id: { column: "id", id: true },
+		first_name: { column: "first_name" },
+		last_name: { column: "last_name" },
+		avatar_url: { column: "avatar_url" },
+		email: { column: "email" },
+		provider: { column: "provider" },
+		bod: { column: "bod" },
+		verified: { column: "verified" },
+		joined_at: { column: "created_at" }
+	}
+]
 
 module.exports = userDefinition
