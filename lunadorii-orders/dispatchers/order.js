7fdb8414351e3bd11eaaeb3ce7eeb0b06ff89b06
Hyperspace018 <hyperspace018@gmail.com>
2018-09-18 16:31:20 +0700

    Sort recent orders

--- lunadorii-orders/dispatchers/order.js
+++ lunadorii-orders/dispatchers/order.js
@@ -327,17 +327,20 @@ exports.getOrderRecent = id => {
 		let list = data.map(res_data => res_data)
 		list.map(l => {
 			let list_map = l.list
-			list_map.map(res => {
-				console.log(res)
-			})
+			list_map.map(res => ({
+				...res,
+				thumbnails: res.thumbnails.sort(
+					(a, b) => a.product_thumbnail_id - b.product_thumbnail_id
+				)
+			}))
 		})
 	}
 
 	// return thumbnails_map.map(res => ({
-	// 	...res,
-	// 	thumbnails: res.thumbnails.sort(
-	// 		(a, b) => a.product_thumbnail_id - b.product_thumbnail_id
-	// 	)
+		// ...res,
+		// thumbnails: res.thumbnails.sort(
+		// 	(a, b) => a.product_thumbnail_id - b.product_thumbnail_id
+		// )
 	// }))
 
 	return knexRecentOrders("orders.id", id)
