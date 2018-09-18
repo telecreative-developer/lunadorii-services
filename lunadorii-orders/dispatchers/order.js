b4e376996497a9b09dc464aa50c3b6ce8d89688a
Hyperspace018 <hyperspace018@gmail.com>
2018-09-18 16:24:06 +0700

    Sort recent orders

--- lunadorii-orders/dispatchers/order.js
+++ lunadorii-orders/dispatchers/order.js
@@ -327,12 +327,10 @@ exports.getOrderRecent = id => {
 		let list = data.map(res_data => res_data)
 		list.map(l => {
 			let list_map = l.list
-			list_map.map(res => ({
-				...res,
-				thumbnails: res.thumbnails.sort(
-					(a, b) => a.product_thumbnail_id - b.product_thumbnail_id
-				)
-			}))
+			list_map.map(res => {
+				let thumbnail_map = res.thumbnails
+				console.log(thumbnail_map)
+			})
 		})
 
 		// return thumbnails.map(res => ({
