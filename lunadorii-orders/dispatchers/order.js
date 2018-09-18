7b99172e10fa13b534c8e4bdc58aba13f7c5a54d
Hyperspace018 <hyperspace018@gmail.com>
2018-09-18 15:04:11 +0700

    Sort recent orders

--- lunadorii-orders/dispatchers/order.js
+++ lunadorii-orders/dispatchers/order.js
@@ -329,10 +329,21 @@ exports.getOrderRecent = id => {
 }
 
 exports.getOrderRecentSingle = order_id => {
+
+	const sortProductThumbnails = data => {
+		const {list} = data
+		return list.map(res => ({
+			...res,
+			thumbnails: res.thumbnails.sort(
+				(a, b) => a.product_thumbnail_id - b.product_thumbnail_id
+			)
+		}))
+	}
+
 	return knexRecentOrders("orders.order_id", order_id)
 		.then(res => NestHydrationJS.nest(res, historyDefinition))
 		.then(res => midtransStatus(res))
-		// .then(res => sortProductThumbnails(res))
+		.then(res => sortProductThumbnails(res))
 		.then(res => successResponse(res, "Success Get Order Recent", 200))
 }
 
